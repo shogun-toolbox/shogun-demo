8fce12d52b565f6100902284bfe8a95be8884847
foulwall <liuzhengyang@outlook.com>
2013-07-01 01:06:43 +0800

    1. merged the perceptron demo. since in enhanced framework, when creating this demo, no boring js or html needed, just write a python script to tell how to do calculation. 2. make an individual fetch_data package, for read typical data from http request.

---
+++ common/fetch_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+import modshogun as sg
+import json
+def get_binary_features(request):
+    try:
+        class_a_point_set_raw = json.loads(request.POST['mouse_left_click_point_set'])
+        class_b_point_set_raw = json.loads(request.POST['mouse_right_click_point_set'])
+    except:
+        raise ValueError("cannot read click pts")
+    class_a_point_set = []
+    class_b_point_set = []
+    for point in class_a_point_set_raw:
+        class_a_point_set.append([point['x'], point['y']])
+    for point in class_b_point_set_raw:
+        class_b_point_set.append([point['x'], point['y']])
+    class_a = np.transpose(np.array(class_a_point_set, dtype=float))
+    class_b = np.transpose(np.array(class_b_point_set, dtype=float))
+
+    if not ( len(class_a) + len(class_b)):
+        raise ValueError("labels not enough")
+    else:
+        features = np.concatenate((class_a, class_b), axis = 1)
+        labels = np.concatenate((np.ones(class_a.shape[1]), -np.ones(class_b.shape[1])), axis = 1)
+
+    features = sg.RealFeatures(features)
+    labels = sg.BinaryLabels(labels)
+
+    return features, labels
