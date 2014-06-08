94bac4c1fc35d680fcf2718df9db329ead25221a
Saurabh <saurabh.mahindre@gmail.com>
2014-06-08 14:33:20 +0530

    regression and dataset

--- demos/regression/regression.py
+++ demos/regression/regression.py
@@ -12,55 +12,46 @@ def handler(request):
     if request.method == 'GET':
         return entrance(request)
     else:
-        return support_vector_regression(request)
+        return regression(request)
 
 def entrance(request):
     arguments = [
         {
             'argument_type': 'select',
-            'argument_name': 'kernel',
-            'argument_items': ['GaussianKernel', 'PolynomialKernel', 'LinearKernel' ],
-            'argument_default': 'GaussianKernel',
-            'argument_explain': '<i>Kernel</i> Function',
-        },
-        {
-            'argument_type': 'decimal',
-            'argument_name': 'C',
-            'argument_default': '1.2',
-            'argument_explain': 'Penalty parameter of the error term'
-        },
-        {
-            'argument_type': 'decimal',
-            'argument_name': 'tube',
-            'argument_default': '0.04',
-            'argument_explain': 'Specifies the allowed deviation of the prediction from the actual value'
+            'argument_label': 'Regression',
+            'argument_name': 'regression',
+            'argument_items': ['LeastSquaresRegression',
+                               'LinearRidgeRegression',
+                               'KernelRidgeRegression'],
+            'argument_explain': 'Regression tool'
         },
         {
             'argument_type': 'decimal',
             'argument_name': 'sigma',
             'argument_default': '0.3',
-            'argument_explain': 'The sigma to use in the GaussianKernel'
+            'argument_explain': 'For GaussianKernel (KernelRidgeRegression)'
         },
         {
-            'argument_type': 'integer',
-            'argument_name': 'degree',
+            'argument_type': 'decimal',
+            'argument_name': 'Tau',
             'argument_default': '5',
-            'argument_explain': 'The degree to use in the PolynomialKernel'
+            'argument_explain': 'tau to use in the (Kernel)RidgeRegression'
         },
         {
             'argument_type': 'button-group',
             'argument_items': [{'button_name': 'regress',
                                 'button_type': 'json_up_down_load'},
                                {'button_name': 'clear'}]
-        }
-        ]
+        }]
+
     toy_data_arguments = [
         {
             'problem_type': 'regression',
             }
         ]
+
         
-    properties = { 'title': 'Supported Vector Regression',
+    properties = { 'title': 'Regression',
                    'template': {'type': 'coordinate-2dims',
                                 'heatmap': False,
                                 'coordinate_system': {'horizontal_axis': {'position': 'bottom',
@@ -81,32 +72,47 @@ def entrance(request):
                            'panel_name': 'toy_data',
                            'panel_label': 'toy data',
                            'panel_property': toy_data_arguments
-                       }]}
-    return render_to_response("regression/support_vector_regression.html",
+                       }],
+                    'data_sets' : ['boston_housing']
+                       }
+    return render_to_response("regression/regression.html",
                               properties,
                               context_instance=RequestContext(request))
     
-def support_vector_regression(request):
-    try:
-        arguments=_read_data(request)
-        svm=_train_svr(*arguments)
-        domain = json.loads(request.POST['axis_domain'])
-        x=np.linspace(domain['horizontal'][0], domain['horizontal'][1], 100)
-        y=np.array(svm.apply(sg.RealFeatures(np.array([x]))).get_labels(), dtype=np.float64)
-        line_dot = []
-        for i in xrange(len(x)):
-            line_dot.append({'x' : x[i], 'y' : y[i]})
-        return HttpResponse(json.dumps(line_dot))
-    except:
-        raise Http404
+def regression(request):
+    #try:
+    domain = json.loads(request.POST['axis_domain'])        
+    X = np.linspace(domain['horizontal'][0], domain['horizontal'][1], 100) 
+    x = np.array([X])
+    feat = sg.RealFeatures(x)
+
+    arguments=_read_data(request)
+    
+    tool=request.POST['regression']
+    if (tool == 'LeastSquaresRegression'):
+        ls = _train_ls(*arguments)
+        y = _apply_ls(feat, ls)
+    
+    elif (tool == 'LinearRidgeRegression'):
+        lrr = _train_lrr(*arguments)
+        y = _apply_lrr(feat, lrr)
+
+    elif (tool=='KernelRidgeRegression'):
+        krr, kernel, train =_train_krr(*arguments)
+        y = _apply_krr(kernel, train, feat, krr)
+
+    line_dot = []
+    for i in xrange(len(X)):
+        line_dot.append({'x' : X[i], 'y' : y[i]})
+    return HttpResponse(json.dumps(line_dot))
+    #except:
+    #   raise Http404
             
 def _read_data(request):
     labels = []
     features = []
     data = json.loads(request.POST['point_set'])
-    cost = float(request.POST['C'])
-    tubeeps = float(request.POST['tube'])
-    kernel_name = request.POST['kernel']
+    tau = float(request.POST['Tau'])
     for pt in data:
         labels.append(float(pt["y"]))
         features.append(float(pt["x"]))
@@ -121,11 +127,38 @@ def _read_data(request):
     
     lab = sg.RegressionLabels(labels)
     train = sg.RealFeatures(examples)
-    kernel = get_kernel(request, train)
-    return (cost, tubeeps, lab, kernel)
+    
+    sigma = float(request.POST["sigma"])
+    kernel = sg.GaussianKernel(train, train, sigma)
+
+    return (tau, lab, kernel, train)
                 
-def _train_svr(cost, tubeeps, lab, kernel):
-    svm = sg.LibSVR(cost, tubeeps, kernel, lab)
-    svm.train()
-    svm.set_epsilon(1e-2)
-    return svm
+def _train_krr(tau, lab, kernel, train):
+    krr = sg.KernelRidgeRegression(tau, kernel, lab)
+    krr.train(train)
+    return krr, kernel, train
+
+def _apply_krr(kernel, train, feat, krr):
+    kernel.init(train, feat)
+    y = np.array(krr.apply().get_labels(), dtype=np.float64)
+    return y
+
+
+def _train_ls(tau, lab, kernel, train):
+    ls = sg.LeastSquaresRegression(train, lab)
+    ls.train()
+    return ls
+
+def _apply_ls(feat, ls):
+    y = np.array(ls.apply(feat).get_labels(), dtype=np.float64)
+    return y
+
+def _train_lrr(tau, lab, kernel, train):
+    lrr = sg.LinearRidgeRegression(tau, train, lab)
+    lrr.train()
+    return lrr
+
+def _apply_lrr(feat, lrr):
+    y = np.array(lrr.apply(feat).get_labels(), dtype=np.float64)
+    return y
+
--- urls.py
+++ urls.py
@@ -5,11 +5,12 @@ urlpatterns = patterns('demos',
     
     url(r'^regression/svr/', 'regression.svr.handler'),
     url(r'^regression/gp/', 'regression.gp.handler'),
+    url(r'^regression/regression/', 'regression.regression.handler'),
     
     url(r'^classifier/binary/', 'classifier.binary.handler'),
     url(r'^classifier/perceptron/', 'classifier.perceptron.handler'),
     url(r'^classifier/multiclass/', 'classifier.multiclass.handler'),
-    url(r'^classifier/gp', 'classifier.gp.handler'),
+    url(r'^classifier/gp/', 'classifier.gp.handler'),
     
     url(r'^dimred/tapkee/$', 'dimred.tapkee.entrance'),
     url(r'^dimred/tapkee/words.json', 'dimred.tapkee.words'),
@@ -27,6 +28,7 @@ urlpatterns = patterns('demos',
 urlpatterns += patterns('util',
     url(r'^toy_data/generator/generate', 'generator.generate'),
     url(r'^toy_data/importer/dump', 'importer.dump'),
+    url(r'^toy_data/importer/regress_dump', 'importer.regress_dump'),   
     url(r'^toy_data/importer/files', 'importer.files'),
     url(r'^data/(\w+)/(.+)', 'data.serve'),
 )                                                            
